154476f78c03bcb223e69a005399941fd57050b7
MzTechnology97 <86824210+MzTechnology97@users.noreply.github.com>
2022-04-27 21:16:39 +0200

    fix check  update miner

--- monitor-scripts/update-check.sh
+++ monitor-scripts/update-check.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-wget --no-cache https://raw.githubusercontent.com/briffy/PiscesQoLDashboard/main/version -O /var/dashboard/update
+latest=$(curl -s https://quay.io/api/v1/repository/team-helium/miner?tab=tags | grep -Po 'miner-arm64_[0-9]+\.[0-9]+\.[0-9]+\.[^"]+_GA' | sort -n | tail -1)
+
+echo $latest > /var/dashboard/statuses/latest_miner_version
+docker ps --format "{{.Image}}" --filter "name=miner" | grep -Po "miner-arm64_.*" > /var/dashboard/statuses/current_miner_version
